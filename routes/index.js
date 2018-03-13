4b48fdb60c8d5093d5978199eeec71079f974322
Jagdsh <jagdshlk@thoughtworks.com>
2018-03-13 11:37:09 +0530

    SVA-0002: Addig cors

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,10 @@ router.get('/', function(req, res, next) {
 
 router.post('/analytics-data', function(req, res, next) {
   	output = decode(req.body['data']);
+  	res.header("Access-Control-Allow-Origin", "*");
+  	res.header('Access-Control-Allow-Methods', 'POST');
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
 	console.log(output);
 
 });
