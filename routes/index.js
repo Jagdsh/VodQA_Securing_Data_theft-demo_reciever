f48fbaa2538245754a498543246babc4c360eeb9
Jagdsh lk Chand <jagdshlkchand@INJagdsh.local>
2018-09-08 01:41:13 +0530

    SVA-003: getting data from headers <Jagdsh>

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var atob = require('atob');
 var router = express.Router();
 
 /* GET home page. */
@@ -8,77 +9,20 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/analytics-data', function(req, res, next) {
-  	output = decode(req.body['data']);
-  	console.log(req.get('Content-MD5'));
+  	output = decodeatob(JSON.parse(JSON.stringify(req.headers))['content-md5']);
   	res.header("Access-Control-Allow-Origin", "*");
   	res.header('Access-Control-Allow-Methods', 'POST');
   	res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
+	res.end();
 	console.log(output);
-
 });
 
-function decode(input){
-	var output = "";
-	var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
-	var chr1, chr2, chr3;
-	var enc1, enc2, enc3, enc4;
-	var i = 0;
+router.post('/google-analytics', function(req, res, next) {
+  res.end();
+});
 
-	input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-	while (i < input.length) {
-
-	    enc1 = _keyStr.indexOf(input.charAt(i++));
-	    enc2 = _keyStr.indexOf(input.charAt(i++));
-	    enc3 = _keyStr.indexOf(input.charAt(i++));
-	    enc4 = _keyStr.indexOf(input.charAt(i++));
-
-	    chr1 = (enc1 << 2) | (enc2 >> 4);
-	    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-	    chr3 = ((enc3 & 3) << 6) | enc4;
-
-	    output = output + String.fromCharCode(chr1);
-
-	    if (enc3 != 64) {
-	        output = output + String.fromCharCode(chr2);
-	    }
-	    if (enc4 != 64) {
-	        output = output + String.fromCharCode(chr3);
-    	}
-	}
-
-	return _utf8_decode(output);
-}
-
-function _utf8_decode(utftext) {
-	var string = "";
-	var i = 0;
-	var c = c1 = c2 = 0;
-
-	while (i < utftext.length) {
-
-	    c = utftext.charCodeAt(i);
-
-	    if (c < 128) {
-	        string += String.fromCharCode(c);
-	        i++;
-	    }
-	    else if ((c > 191) && (c < 224)) {
-	        c2 = utftext.charCodeAt(i + 1);
-	        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-	        i += 2;
-	    }
-	    else {
-	        c2 = utftext.charCodeAt(i + 1);
-	        c3 = utftext.charCodeAt(i + 2);
-	        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-	        i += 3;
-	    }
-
-	}
-
-	return string;
+function decodeatob(input){
+	return JSON.parse(atob(input));
 }
 
 module.exports = router;
